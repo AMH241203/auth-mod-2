a29e4a0dc6c44cee8d3b8d55e5653c41c3b93ea3
Ahnaf Mahmud <mahmud.ahnaf2003@gmail.com>
2024-07-15 15:13:05 +0600

    Terraform Updates

--- phones/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.inventory)
-admin.site.register(models.phone_info)
--- phones/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('', views.create_view, name='create_view'),
-        path('inventory', views.list_view, name='list_view')
-    ]
